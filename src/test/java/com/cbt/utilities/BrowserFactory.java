80e3a05ae0fb2be1be84afdb778c82b951d971e4
Halis-Can <haliscan@hotmail.com>
2020-03-11 22:21:54 -0400

    Homework1

--- src/test/java/com/cbt/utilities/BrowserFactory.java
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -1,7 +1,36 @@
 package com.cbt.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
 public class BrowserFactory {
-    public static void main(String[] args) {
-        System.out.println("hello selenium");
+    /**your homework will be to improve this method!
+     *This method return webdriver object based on browser type
+     *If you want to use chrome browser,just provide chrome as a parameter
+     *
+     * @param browser
+     * @return webdriver object
+     */
+    public static WebDriver getDriver(String browser){
+        if(browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().version("79.0").setup();
+            return new ChromeDriver();
+        }else if(browser.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        }else if (browser.equalsIgnoreCase("Opera")){
+            WebDriverManager.operadriver().setup();
+            return new OperaDriver();
+        }else if (browser.equalsIgnoreCase("IE")){
+            WebDriverManager.iedriver().setup();
+            return new InternetExplorerDriver();
+        }
+        else{
+            return null;
+        }
     }
+
 }
---
+++ src/test/java/com/cbt/utilities/StringUtility.java
@@ -0,0 +1,19 @@
+package com.cbt.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StringUtility {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver = new ChromeDriver();
+    }
+    public static void verifyEquals(String expected, String actual) {
+        if (expected.equals(actual)) {
+            System.out.println("pass");
+        } else {
+            System.out.println("failed");
+        }
+    }
+}
